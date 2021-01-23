544960d2ec96b8ff60a1eca67cdb3d6a2a176ce8
CanCumaYaman <65663050+CanCumaYaman@users.noreply.github.com>
2021-01-23 19:07:13 +0300

    Add project files.

---
+++ UsingDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UsingDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(01, "Adana");
+            myDictionary.Add(06, "Ankara");
+            myDictionary.Add(38, "Kayseri");
+            myDictionary.Print();
+        }
+    }
+}
